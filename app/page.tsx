b36aa5b1277849c20e5d04b713bc6cd736889258
snowcodeer <nataliechan1963@gmail.com>
2025-09-08 14:53:23 +0100

    Initial commit: VisualAIser project

---
+++ app/page.tsx
@@ -0,0 +1,39 @@
+// highlight-start
+import { fetchPersonas } from "@speechmatics/flow-client-react";
+import { Controls } from "@/components/Controls";
+import { Status } from "@/components/Status";
+import { TranscriptView } from "@/components/TranscriptView";
+// highlight-end
+import { Providers } from "./providers";
+
+export default async function Home() {
+  // highlight-start
+  const fetchedPersonas = await fetchPersonas();
+  
+  // Add custom template ID from environment variables
+  const customTemplateId = process.env.CUSTOM_TEMPLATE_ID;
+  const customTemplateName = process.env.CUSTOM_TEMPLATE_NAME || "My Custom Template";
+  
+  const personas = {
+    ...fetchedPersonas,
+    ...(customTemplateId && { [customTemplateId]: { name: customTemplateName } })
+  };
+  // highlight-end
+
+  return (
+    <main>
+      <h1>Speechmatics NextJS Flow Example</h1>
+      <Providers>
+        {/* highlight-start */}
+        <div className="flex flex-col gap-4 h-full min-h-0">
+          <div className="flex flex-row gap-2">
+            <Controls personas={personas} />
+            <Status />
+          </div>
+          <TranscriptView />
+        </div>
+        {/* highlight-end */}
+      </Providers>
+    </main>
+  );
+}
